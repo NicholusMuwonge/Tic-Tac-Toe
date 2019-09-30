2fdc04721037d924c88ae0c264f455363ddc790e
NicholusMuwonge <nicholusmuwonge@gmail.com>
2019-09-30 23:29:58 +0300

    story(table): table display
    - creating how the table will be displayed in the terminal
    - create welcoming message

---
+++ Game/App.py
@@ -0,0 +1,34 @@
+from beautifultable import BeautifulTable
+
+class Structure:
+
+    # create how the board will display like in the terminal
+    def __init__(self):
+        self.table = BeautifulTable()
+
+    def board_structure(self, board):
+        table = BeautifulTable()
+        # table represented by positional values
+        table.append_row([board[7], board[8], board[9]])
+        table.append_row([board[4], board[5], board[6]])
+        table.append_row([board[1], board[2], board[3]])
+        print(table)
+
+    def welcome(self):
+        print('Welcome to Tic Tac Toe, My Version!')
+
+
+class Prompts:
+
+    # add prompts for user to choose character
+    def chooseCharacter(self):
+        # choose letter
+        letter = None
+        while not (letter == 'Y' or letter == 'A'):
+            print('Do you want to be Y or A?')
+            letter = input().upper()
+        # the index[0] is the player
+        if letter == 'Y':
+            return ['Y', 'A']
+        else:
+            return ['A', 'Y']
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Tic-Tac-Toe
-Its Two player game, where you play against the computer fill out any of the spaces until one has a line of consecutive letters aligned to become a winner. One can either win, lose or draw.
+```Its Two player terminal game, where you play against the computer fill out any of the spaces until one has a line of consecutive letters aligned to become a winner. One can either win, lose or draw.```
+
+
+#### Directions
+
+
+#### Where the application is hosted
+Github - https://github.com/NicholusMuwonge/Tic-Tac-Toe/
